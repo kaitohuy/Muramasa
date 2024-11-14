6517fa44a3d3f66460ba86d5a72e05baa024ac2d
kaitohuy <huynguyendoan0305@gmail.com>
2024-11-14 18:10:59 +0700

    render

--- Muramasa/src/data/DatabaseConnection.java
+++ Muramasa/src/data/DatabaseConnection.java
@@ -5,10 +5,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:postgresql://localhost:5432/Muramasa";
+//    private static final String URL = "jdbc:postgresql://localhost:5432/Muramasa";
     private static final String USER = "postgres";
     private static final String PASSWORD = "081204";
-
+    private static final String URL = System.getenv("DATABASE_URL");
+    
     public Connection getConnection() {
         Connection connection = null;
         try {
